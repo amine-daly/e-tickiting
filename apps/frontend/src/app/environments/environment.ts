73a33bd35be3946e15de8d81047056d6908569bb
amine.dali <amine.dali@diktup.com>
2026-05-16 16:53:33 +0100

    env

--- apps/frontend/src/app/environments/environment.ts
+++ apps/frontend/src/app/environments/environment.ts
@@ -1,7 +1,12 @@
+export const LOGO_BASE =
+  'https://eticketing-app.s3.eu-north-1.amazonaws.com/logos/logo_base.png';
+export const LOGO_DARK =
+  'https://eticketing-app.s3.eu-north-1.amazonaws.com/logos/logo_dark.png';
+
 export const environment = {
   production: false,
   appVersion: 'v1',
   USERDATA_KEY: 'auth',
-  /* apiBase: 'http://localhost:8080/api', */
-  apiBase: 'http://13.61.196.160/api',
+  apiBase: 'http://localhost:8080/api',
+  /* apiBase: 'http://13.61.196.160/api', */
 };
